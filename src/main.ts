b99dded250c9dd0f1f74322adc0ee7c9bda93163
d4n-c4stillo <d4n_c4stillo@outlook.com>
2024-05-06 00:09:41 -0400

    3000

--- src/main.ts
+++ src/main.ts
@@ -73,7 +73,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   //await app.listen(configService.get('APP_PORT'));
-  await app.listen(configService.get('APP_PORT'),'0.0.0.0');
+  await app.listen(3000);
   console.log('init at port 3000 !!');
 }
 bootstrap();
